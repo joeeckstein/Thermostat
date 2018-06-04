a71f2a16c34af3cbf0dd7745c9744bbb0810b3db
Joe Eckstein <eckstein.joseph@gmail.com>
2018-06-04 02:17:30 -0700

    Initial commit

---
+++ Thermostat.ino
@@ -0,0 +1,72 @@
+#include <Wire.h>
+#include <Adafruit_MPL3115A2.h>
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+const int relayPin = 7;
+const int setLow   = 17;
+const int setHigh  = 25;
+
+// Power by connecting Vin to 3-5V, GND to GND
+// Uses I2C - connect SCL to the SCL pin, SDA to SDA pin
+// See the Wire tutorial for pinouts for each Arduino
+// http://arduino.cc/en/reference/wire
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+void setup() {
+  //Serial.begin(9600);
+  //while (!Serial){
+    ; // Wait until serial connects
+  //}
+  //Serial.println("Adafruit_MPL3115A2 test!");
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.clear();
+  lcd.print("Loading... v.4");
+  pinMode(relayPin, OUTPUT);
+  delay(1000);
+  lcd.clear();
+}
+
+void loop() {
+  if (! baro.begin()) {
+    lcd.clear();
+    lcd.print("Couldnt find sensor");
+    return;
+  }
+  
+  float pascals = baro.getPressure();
+  // Our weather page presents pressure in Inches (Hg)
+  // Use http://www.onlineconversion.com/pressure.htm for other units
+  //Serial.print(pascals/3377); Serial.println(" Inches (Hg)");
+  
+  //float altm = baro.getAltitude();
+  //Serial.print(altm); Serial.println(" meters");
+  
+  float tempC = baro.getTemperature();
+  //Serial.print(tempC); Serial.println("*C");
+
+  lcd.setCursor(0,0);
+  lcd.print(tempC); lcd.print((char)223);lcd.print("C ");
+
+  if (tempC > setLow && tempC < setHigh){
+    digitalWrite(relayPin, HIGH);
+    //Serial.println("ON");
+    lcd.setCursor(0,1);
+    lcd.print("Relay:ON ");
+  }
+  else {
+    digitalWrite(relayPin, LOW);
+    //Serial.println("OFF");
+    lcd.setCursor(0,1);
+    lcd.print("Relay:OFF");
+  }
+
+  lcd.setCursor(12,0);
+  lcd.print("L:"); lcd.print(setLow);
+  lcd.setCursor(12,1);
+  lcd.print("H:"); lcd.print(setHigh);
+  delay(900000);
+}
